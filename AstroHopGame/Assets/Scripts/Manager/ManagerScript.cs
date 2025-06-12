bd3d8d52f07806903daa4555fd6bc881a67d7561
AaronPloerer <aaron.plorer@gmail.com>
2025-06-12 11:53:28 -0400

    click error fix

--- AstroHopGame/Assets/Scripts/Manager/ManagerScript.cs
+++ AstroHopGame/Assets/Scripts/Manager/ManagerScript.cs
@@ -127,7 +127,10 @@ public class ManagerScript : MonoBehaviour
     public void LanguageDropdown()
     {
         // Add click sound when selecting language
-        AudioManagerScript.instance.PlaySFX(AudioManagerScript.instance.click, AudioManagerScript.instance.clickVolume);
+        if (MenuUIScript.instance.optionsPanel.activeSelf)
+        {
+            AudioManagerScript.instance.PlaySFX(AudioManagerScript.instance.click, AudioManagerScript.instance.clickVolume);
+        }
 
         // Load language from UI language dropdown
         int selectedLocale = MenuUIScript.instance.languageDropdown.value;
